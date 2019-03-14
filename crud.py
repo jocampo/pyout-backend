3295d565c63ac1f23b128e5201b4fbdc16058703
Giuseppe <piccolino96@gmail.com>
2019-03-14 16:16:47 -0500

    Created Requirements.txt file

--- crud.py
+++ crud.py
@@ -1,9 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 
 
 class Crud:
     def start(self,url = "postgres://admin123:admin123@sqlito.csvdz1qsdaks.us-east-1.rds.amazonaws.com:5432/postgres"):
         db = create_engine(url)
+        metadata = MetaData()
+        print(metadata)
 
     def addAddSets(self):
         pass
