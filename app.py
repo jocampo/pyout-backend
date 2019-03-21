7198ea764d61be4aa543c239e2764c7c8700666f
Jorge Ocampo <jocampo@belatrixsf.com>
2019-03-21 14:41:10 -0500

    Adds the AdSets controller boilerplate

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask_restful import Api
 
 from src.api.controllers.gender_controller import GenderController
 from src.api.controllers.geolocations_controller import GeolocationsController
-from src.api.controllers.platforms_controller import PlatformController
+from src.api.controllers.platforms_controller import PlatformsController
+from src.api.controllers.adsets_controller import AdsetsController
 
 
 # Create the application instance
@@ -14,7 +15,8 @@ api = Api(app, "/api")
 
 api.add_resource(GenderController, "/genders")
 api.add_resource(GeolocationsController, "/geolocations")
-api.add_resource(PlatformController, "/platforms")
+api.add_resource(PlatformsController, "/platforms")
+api.add_resource(AdsetsController, "/adsets")
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
---
+++ src/api/controllers/adsets_controller.py
@@ -0,0 +1,15 @@
+from flask_restful import Resource
+from src.db.db_wrapper import DBWrapper
+from flask_jsonpify import jsonify
+
+
+class AdsetsController(Resource):
+    def get(self):
+        """Returns all adsets from the database"""
+        db_wrapper = DBWrapper()
+        adsets_response = db_wrapper.get_all_adsets()
+        return jsonify({'adsets': [dict(row) for row in adsets_response]})
+
+    def post(self):
+        """Creates an adset"""
+        return {"status": "Success"}, 201
