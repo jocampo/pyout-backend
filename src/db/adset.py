67d453354fd0fa7680e31eb68c2df7e9dd2ba91b
Jorge Ocampo <jocampo@belatrixsf.com>
2019-03-22 11:58:28 -0500

    Fixes to the create AdSet method

--- src/db/adset.py
+++ src/db/adset.py
@@ -13,7 +13,7 @@ class Adset:
 
     def list_all_adsets(self):
         platform = db.Table('adsets', self._metadata, autoload=True,
-                          autoload_with=self._engine)
+                            autoload_with=self._engine)
         query = db.select([platform])
         connection = self._engine.connect()
         result_proxy = connection.execute(query)
@@ -24,16 +24,20 @@ class Adset:
     def create_adset(self,platform_id,gender_id, geolocation_id):
         if self._name and self._active and platform_id and gender_id and geolocation_id:
             adsets = db.Table('adsets', self._metadata, autoload=True,
-                                autoload_with=self._engine)
+                              autoload_with=self._engine)
             query = db.insert(adsets)
             values_list = [
                 {'name': self._name , 'active' : self._active}, ]
             connection = self._engine.connect()
             result_proxy = connection.execute(query, values_list)
-            result_set = result_proxy.fetchall()
-            print(result_set)
-            # Pending ids.
+            adset_id = result_proxy.inserted_primary_key[0]
 
+            if adset_id:
+                self.create_adset_genders(adset_id=adset_id,gender_id=gender_id)
+                self.create_adset_geolocation(adset_id=adset_id,geolocation_id=geolocation_id)
+                self.create_adset_platforms(adset_id=adset_id,platform_id=platform_id)
+            else:
+                print("Inserted AdSet Id is invalid")
 
     def set_active(self,active):
         if active:
@@ -43,8 +47,32 @@ class Adset:
         if name:
             self._name = name
 
-    def create_adset_geolocation (adset_id,geolocation_id):
-        pass
+    def create_adset_geolocation (self,adset_id,geolocation_id):
+        adsets = db.Table('adsets_geolocations', self._metadata, autoload=True,
+                          autoload_with=self._engine)
+        query = db.insert(adsets)
+        values_list = [
+            {'adset_id': adset_id, 'geolocation_id':geolocation_id}, ]
+        connection = self._engine.connect()
+        result_proxy = connection.execute(query, values_list)
+        return result_proxy.inserted_primary_key
 
-    def create_adset_genders (adset_id,geolocation_id):
-        pass
+    def create_adset_genders (self,adset_id,gender_id):
+        adsets = db.Table('adsets_genders', self._metadata, autoload=True,
+                          autoload_with=self._engine)
+        query = db.insert(adsets)
+        values_list = [
+            {'adset_id': adset_id, 'gender_id': gender_id}, ]
+        connection = self._engine.connect()
+        result_proxy = connection.execute(query, values_list)
+        return result_proxy.inserted_primary_key
+
+    def create_adset_platforms (self,adset_id,platform_id):
+        adsets = db.Table('adsets_platforms', self._metadata, autoload=True,
+                          autoload_with=self._engine)
+        query = db.insert(adsets)
+        values_list = [
+            {'adset_id': adset_id, 'platform_id': platform_id}, ]
+        connection = self._engine.connect()
+        result_proxy = connection.execute(query, values_list)
+        return result_proxy.inserted_primary_key
